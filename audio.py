553ee6bf2c4ae1c2a4487425cd53ff3072685ac2
Perdolee <sjfh@yandex.ru>
2018-07-14 19:33:05 +0500

    Upgrade
    + Added Two-factor authentication
    + Added Captcha handler
    + Folder creation if not exists
    + All audio list
    + Output file namecheck
    + PEP8 improvement

--- audio.py
+++ audio.py
@@ -1,20 +1,48 @@
 #!/usr/bin/env python3
 
+import getpass
+import os.path
+import re
 import shutil
 import time
-import getpass
+
 import requests
-import os.path
 import vk_api
 from vk_api.audio import VkAudio
 
 DOWNLOAD_DIR = './download'
+if not os.path.exists(DOWNLOAD_DIR):
+    os.makedirs(DOWNLOAD_DIR)
+
+
+def auth_handler():
+    """ При двухфакторной аутентификации вызывается эта функция.
+    """
+
+    # Код двухфакторной аутентификации
+    key = input("Enter authentication code: ")
+    # Если: True - сохранить, False - не сохранять.
+    remember_device = True
+
+    return key, remember_device
+
+
+def captcha_handler(captcha):
+    """ При возникновении капчи вызывается эта функция и ей передается объект
+        капчи. Через метод get_url можно получить ссылку на изображение.
+        Через метод try_again можно попытаться отправить запрос с кодом капчи
+    """
+
+    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
+
+    # Пробуем снова отправить запрос с капчей
+    return captcha.try_again(key)
 
 
 def main():
     login, password = input('Login: '), getpass.getpass()
 
-    vk_session = vk_api.VkApi(login, password)
+    vk_session = vk_api.VkApi(login, password, auth_handler=auth_handler, captcha_handler=captcha_handler)
     vk = vk_session.get_api()
 
     try:
@@ -23,23 +51,33 @@ def main():
         print(error_msg)
         return
 
-    vkaudio = VkAudio(vk_session)
+    vk_audio = VkAudio(vk_session)
 
-    id = vk.users.get()[0]['id']
-    audios = vkaudio.get(owner_id=id)
+    user_id = vk.users.get()[0]['id']
+    audios = []
+    try:
+        print('Getting audio playlist, please wait')
+        for track in vk_audio.get_iter(user_id):
+            audios.append(track)
+    except AttributeError as error_msg:
+        print(error_msg)
+        print('Please upgrade vk_api library. Use: "pip install vk_api --upgrade --user"')
+        return
 
     for audio in audios:
         artist = audio['artist'].replace('/', '')
         title = audio['title'].replace('/', '')
         url = audio['url']
-
         file_name = '/'.join((DOWNLOAD_DIR, '{} - {}.mp3'.format(artist, title)))
+        re.sub('[^\w\-_.]', '_', file_name)
         if not os.path.isfile(file_name):
             response = requests.get(url, stream=True)
-            print('Downloading: {}'.format(file_name))
-            with open(file_name, 'wb') as f:
-                shutil.copyfileobj(response.raw, f)
-
+            print('Downloading: {}'.format(file_name), sep=' ')
+            try:
+                with open(file_name, 'wb') as f:
+                    shutil.copyfileobj(response.raw, f)
+            except Exception as error_msg:
+                print(error_msg)
             print('OK')
             time.sleep(2)
 

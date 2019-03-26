1321346d0e10fe215867e86bc8e9023ae505b5d3
Perdolee <sjfh@yandex.ru>
2019-03-26 18:08:28 +0500

    Update

--- audio.py
+++ audio.py
@@ -16,26 +16,14 @@ if not os.path.exists(DOWNLOAD_DIR):
 
 
 def auth_handler():
-    """ При двухфакторной аутентификации вызывается эта функция.
-    """
-
-    # Код двухфакторной аутентификации
     key = input("Enter authentication code: ")
-    # Если: True - сохранить, False - не сохранять.
     remember_device = True
 
     return key, remember_device
 
 
 def captcha_handler(captcha):
-    """ При возникновении капчи вызывается эта функция и ей передается объект
-        капчи. Через метод get_url можно получить ссылку на изображение.
-        Через метод try_again можно попытаться отправить запрос с кодом капчи
-    """
-
     key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
-
-    # Пробуем снова отправить запрос с капчей
     return captcha.try_again(key)
 
 
@@ -53,23 +41,15 @@ def main():
 
     vk_audio = VkAudio(vk_session)
 
-    user_id = vk.users.get()[0]['id']
-    audios = []
-    try:
-        print('Getting audio playlist, please wait')
-        for track in vk_audio.get_iter(user_id):
-            audios.append(track)
-    except AttributeError as error_msg:
-        print(error_msg)
-        print('Please upgrade vk_api library. Use: "pip install vk_api --upgrade --user"')
-        return
+    choice = input('Enter 0 to download your audios or write user_id or group_id without "id" to download other people music: ')
 
-    for audio in audios:
+    user_id = vk.users.get()[0]['id'] if choice == '0' else choice
+
+    for audio in vk_audio.get_iter(user_id):
         artist = audio['artist'].replace('/', '')
         title = audio['title'].replace('/', '')
         url = audio['url']
         file_name = '/'.join((DOWNLOAD_DIR, '{} - {}.mp3'.format(artist, title)))
-        re.sub('[^\w\-_.]', '_', file_name)
         if not os.path.isfile(file_name):
             response = requests.get(url, stream=True)
             print('Downloading: {}'.format(file_name), sep=' ')
@@ -79,8 +59,6 @@ def main():
             except Exception as error_msg:
                 print(error_msg)
             print('OK')
-            time.sleep(2)
-
 
 if __name__ == '__main__':
     main()
